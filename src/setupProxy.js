3f9bb0f7b8bc49a81843d794469ccee66b1af04f
moovendan mahalakshmi <karthikraj1995@gmail.com>
2020-05-25 20:28:09 +0530

    proxy middleware

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+import {createProxyMiddleware} from 'http-proxy-middleware'
+
+module.exports=function(app){
+    app.use("/api",
+        createProxyMiddleware({
+            "target": "https://career-ninja-got.herokuapp.com",
+            "changeOrigin": true
+        })
+    )
+}
