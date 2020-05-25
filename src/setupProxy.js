8bf5ca6ce67e2400b790a8be30ebec931552d788
moovendan mahalakshmi <karthikraj1995@gmail.com>
2020-05-25 22:19:21 +0530

    proxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware')
 module.exports= function(app){
     app.use("/",
         createProxyMiddleware({
-            target: "http://career-ninja-got.herokuapp.com",
+            target: "https://career-ninja-got.herokuapp.com",
             changeOrigin: true
         })
     )
