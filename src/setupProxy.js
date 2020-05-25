3dafa87f5a87750247a680ffc46bf7f67a25de48
KarthikrajS <KarthikrajS>
2020-05-26 04:38:35 +0530

    git deploy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware')
 module.exports= function(app){
     app.use("/",
         createProxyMiddleware({
-            target: "https://career-ninja-got.herokuapp.com",
+            target: "career-ninja-got.herokuapp.com",
             changeOrigin: true
         })
     )
