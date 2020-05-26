afa4df86652965806241239e0db5e2b78429988c
KarthikrajS <KarthikrajS>
2020-05-26 08:36:58 +0530

    api

--- src/components/actions/api.js
+++ src/components/actions/api.js
@@ -3,22 +3,22 @@ import axios from 'axios';
 export default{
     battleData:{
         battleDataList:()=>
-            axios.post("/api/battleData/list").then(res => res.data),
+            axios.post("https://career-ninja-got.herokuapp.com/api/battleData/list").then(res => res.data),
         battleKingsList:()=>
-            axios.post("/api/battleData/kings").then(res => res.data),
+            axios.post("https://career-ninja-got.herokuapp.com/api/battleData/kings").then(res => res.data),
         battleTypesList:()=>
-            axios.post("/api/battleData/types").then(res => res.data),
+            axios.post("https://career-ninja-got.herokuapp.com/api/battleData/types").then(res => res.data),
         battleDataCount:data=>
-            axios.post("/api/battleData/count",{data}).then(res => res.data),
+            axios.post("https://career-ninja-got.herokuapp.com/api/battleData/count",{data}).then(res => res.data),
         battleBasedOnLocation:location=>
-            axios.post("/api/battleData/battleBasedOnLocation",{location}).then(res => res.data),
+            axios.post("https://career-ninja-got.herokuapp.com/api/battleData/battleBasedOnLocation",{location}).then(res => res.data),
         battleBasedOnTypes: type=>
-            axios.post("/api/battleData/battleBasedOnTypes",{type}).then(res => res.data),
+            axios.post("https://career-ninja-got.herokuapp.com/api/battleData/battleBasedOnTypes",{type}).then(res => res.data),
         battleBasedOnKings: king=>
-            axios.post("/api/battleData/battleBasedOnKings",{king}).then(res => res.data),
+            axios.post("https://career-ninja-got.herokuapp.com/api/battleData/battleBasedOnKings",{king}).then(res => res.data),
         battleDetails: battleName=>
-            axios.post("/api/battleData/battleDetails",{battleName}).then(res => res.data),
+            axios.post("https://career-ninja-got.herokuapp.com/api/battleData/battleDetails",{battleName}).then(res => res.data),
         search:data=>
-            axios.post("/api/battleData/search",{data}).then(res=>res.data)
+            axios.post("https://career-ninja-got.herokuapp.com/api/battleData/search",{data}).then(res=>res.data)
     }
 }
